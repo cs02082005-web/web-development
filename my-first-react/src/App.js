8a072ee47239d00b41796cf7c62f3e24867ae6ff
cs02082005-web <cs02082005@gmail.com>
2026-06-11 09:18:25 +0530

    react

---
+++ my-first-react/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Btn from "./button";
+
+function App(){
+
+  return(
+    // HTML Code - 
+    <div>
+    <h1>Hello Guys How are you doing</h1>
+    <button>Login</button>
+    <Btn />
+    <Btn />
+    <Btn />
+
+
+    {/* Every Tag Should Must be Closed */}
+
+    <label>Username</label>
+
+    <input ></input>
+    <Btn />
+    <Btn />
+
+    <Btn />
+    <Btn />
+
+    
+    <Btn />
+
+     </div>
+  )
+}
+
+export default App;
---
+++ my-first-react/src/button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Btn(){
+
+    return(
+        <div>
+            <button style= {{backgroundColor:'yellow',padding:'10px',border:"none"}}>Add To Cart</button>
+        </div>
+    )
+
+}
+
+export default Btn;
